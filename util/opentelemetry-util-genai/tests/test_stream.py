9782774a79f3939951015139eccb587324cdc47a
eternalcuriouslearner <suryateja9108@gmail.com>
2026-05-12 08:28:19 -0400

    polish: fix lint and spdx headers.

--- util/opentelemetry-util-genai/tests/test_stream.py
+++ util/opentelemetry-util-genai/tests/test_stream.py
@@ -1,16 +1,5 @@
 # Copyright The OpenTelemetry Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 # pylint: disable=abstract-class-instantiated
 
@@ -249,7 +238,7 @@ def test_sync_stream_wrapper_exposes_wrapped_stream():
     stream = _FakeSyncStream()
     wrapper = _TestSyncStreamWrapper(stream)
 
-    assert wrapper.__wrapped__ is stream
+    assert getattr(wrapper, "__wrapped__") is stream
 
 
 def test_sync_stream_wrapper_magic_method_passthrough():
@@ -518,7 +507,7 @@ def test_async_stream_wrapper_exposes_wrapped_stream():
     stream = _FakeAsyncStream()
     wrapper = _TestAsyncStreamWrapper(stream)
 
-    assert wrapper.__wrapped__ is stream
+    assert getattr(wrapper, "__wrapped__") is stream
 
 
 def test_async_stream_wrapper_magic_method_passthrough():
